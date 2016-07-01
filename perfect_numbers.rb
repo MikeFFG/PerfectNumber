c99671e3fe4b9765e455656e406d2825262d66c0
Michael Burke <mike.d.burke@gmail.com>
2016-07-01 11:11:12 -0400

    Finish challenge.

---
+++ perfect_numbers.rb
@@ -0,0 +1,24 @@
+# Calculates perfect numbers
+class PerfectNumber
+  def self.sum_of_factors(number)
+    sum = 1
+    (2..number / 2).to_a.each do |num|
+      sum += num if number % num == 0
+    end
+    sum
+  end
+
+  def self.classify(number)
+    raise RuntimeError if number < 0
+
+    factors = sum_of_factors(number)
+    case
+    when factors < number
+      'deficient'
+    when factors == number
+      'perfect'
+    when factors > number
+      'abundant'
+    end
+  end
+end
